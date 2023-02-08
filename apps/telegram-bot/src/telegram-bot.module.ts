7463ba6bfbac571eabc6a4eca633b5435a855c1f
Andrew Buvria <buvrya228@gmail.com>
2023-02-08 20:57:26 +0100

    Revert "Merge remote-tracking branch 'origin/dev' into telegram-api-preparation"
    This reverts commit 4a9c46e712816980a66b2869c75e5ccd56247427, reversing
    changes made to 5a4bcde3ff18463938aa8f76aa9058af50f35890.

--- apps/telegram-bot/src/telegram-bot.module.ts
+++ apps/telegram-bot/src/telegram-bot.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TelegramBotController } from './telegram-bot.controller';
 import { TelegramBotService } from './telegram-bot.service';
+
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-            envFilePath: [`${process.cwd()}/apps/telegram-bot/envs/.env`],
-            isGlobal: true
-        })
-    ],
+    imports: [],
     controllers: [TelegramBotController],
     providers: [TelegramBotService]
 })
--- apps/telegram-bot/src/telegram-bot.service.ts
+++ apps/telegram-bot/src/telegram-bot.service.ts
@@ -1,19 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import * as TelegramBot from 'node-telegram-bot-api';
 
 @Injectable()
 export class TelegramBotService {
-    private readonly TOKEN = this.configEnvService.get('TELEGRAM_BOT_TOKEN');
-    private bot: TelegramBot;
-
-    constructor(
-        private configEnvService: ConfigService
-    ) {
-        this.initSetup();
-    }
-
-    private initSetup(): void {
-        this.bot = new TelegramBot(this.TOKEN, { polling: true });
+    public getHello(): string {
+        return 'Telegram Bot ACUA backend!';
     }
 }
