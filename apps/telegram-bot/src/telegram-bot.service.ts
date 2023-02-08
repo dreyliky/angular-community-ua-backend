03eb16b3d2b5891ae068273a2b93e1a0d4e6d71e
DreyLiky <dreyliky@gmail.com>
2023-02-08 17:34:07 +0200

    Revert "Connect telegram bot"

--- apps/telegram-bot/src/telegram-bot.service.ts
+++ apps/telegram-bot/src/telegram-bot.service.ts
@@ -1,13 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import * as TelegramBot from 'node-telegram-bot-api';
-import { TelegramBotConfigEnum } from './core/enums/telegram-bot-config.enum';
 
 @Injectable()
 export class TelegramBotService {
-    private readonly token: TelegramBotConfigEnum = TelegramBotConfigEnum['Token'];
-    private bot: TelegramBot;
-
-    constructor() {
-        this.bot = new TelegramBot(this.token, { polling: true });
+    public getHello(): string {
+        return 'Telegram Bot ACUA backend!';
     }
 }
