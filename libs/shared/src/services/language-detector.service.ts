63ce7897872e74bb2ed32c6a9f11919a2610a97e
DreyLiky <dreyliky@gmail.com>
2023-02-20 00:36:04 +0200

    fix: language-detector

--- libs/shared/src/services/language-detector.service.ts
+++ libs/shared/src/services/language-detector.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import LanguageDetect from 'languagedetect';
+import type LanguageDetect from 'languagedetect';
 
 @Injectable()
 export class LanguageDetectorService {
-    private readonly detector = new LanguageDetect();
+    private readonly detector: LanguageDetect;
     private readonly ruLanguageKey = 'ru';
 
     constructor() {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        const LanguageDetect = require('languagedetect');
+        this.detector = new LanguageDetect();
+
         this.detector.setLanguageType('iso2');
     }
 
     public isRussian(text: string): boolean {
         const [language] = this.detector.detect(text, 1);
 
-        return language[0] === this.ruLanguageKey;
+        return language?.[0] === this.ruLanguageKey;
     }
 }
