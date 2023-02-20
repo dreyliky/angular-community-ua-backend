5a9236f1d4af654524a9d1d9daa8f4e55d82afc2
Andrew Buvria <buvrya228@gmail.com>
2023-02-20 18:22:50 +0100

    fix(main): make firstName field optional

--- apps/main/src/login/models/login-response.dto.ts
+++ apps/main/src/login/models/login-response.dto.ts
@@ -7,7 +7,6 @@ export class TelegramLoginResponseDto {
     public readonly auth_date: number;
 
     @ApiProperty()
-    @IsNotEmpty()
     public readonly first_name: string;
 
     @ApiProperty()
