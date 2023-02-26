1123e395c06843b5e7a6fe9bdfd0fb2c7ed0b733
DreyLiky <dreyliky@gmail.com>
2023-02-26 18:09:26 +0200

    fix: remove legacy service

--- apps/telegram-bot/src/bot/telegram-bot.module.ts
+++ apps/telegram-bot/src/bot/telegram-bot.module.ts
@@ -1,4 +1,3 @@
-import { LanguageDetectorService } from '@acua/shared';
 import { Module } from '@nestjs/common';
 import { TELEGRAM_BOT_PROVIDER } from './providers';
 import { MessageHandlerFactory } from './services';
@@ -6,7 +5,6 @@ import { TelegramBotService } from './telegram-bot.service';
 
 @Module({
     providers: [
-        LanguageDetectorService,
         TelegramBotService,
         MessageHandlerFactory,
         TELEGRAM_BOT_PROVIDER
