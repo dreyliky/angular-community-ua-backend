84e1845780749e99f3ceed24006535803833b016
DreyLiky <dreyliky@gmail.com>
2023-03-05 19:56:59 +0200

    refactor(main): jwt-strategy

--- apps/main/src/user/jwt-strategy.ts
+++ apps/main/src/user/jwt-strategy.ts
@@ -1,15 +1,12 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
+import { TokenPayload } from '@main/token';
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { UsersService } from './users.service';
+import { AuthorizedUser } from './interfaces';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(
-        private readonly usersService: UsersService,
-        private readonly jwtService: JwtService
-    ) {
+    constructor() {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
@@ -17,16 +14,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         });
     }
 
-    public async validate(data: any): Promise<any> {
-        if (!data) {
-            throw new UnauthorizedException();
-        }
-        const user = await this.usersService.getById(+data.id);
+    public validate(data: TokenPayload): AuthorizedUser {
+        console.log(data);
 
-        if (!user) {
-            throw new UnauthorizedException();
-        }
-
-        return user;
+        return { tgId: data.tgId };
     }
 }
--- apps/main/src/user/user.controller.ts
+++ apps/main/src/user/user.controller.ts
@@ -2,13 +2,18 @@ import { Controller, Get } from '@nestjs/common';
 import { Req, UseGuards } from '@nestjs/common/decorators';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiBearerAuth } from '@nestjs/swagger';
+import { Request } from 'express';
+import { AuthorizedUser } from './interfaces';
 
 @Controller('users')
 export class UserController {
     @Get('me')
     @ApiBearerAuth()
     @UseGuards(AuthGuard('jwt'))
-    public get(@Req() request: any): Promise<unknown> {
+    public get(@Req() request: Request): Promise<unknown> {
+        const authorizedUser = request.user as AuthorizedUser;
+        console.log('authorizedUserInfo', authorizedUser);
+
         return request.body;
     }
 }
