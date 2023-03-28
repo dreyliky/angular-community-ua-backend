cf917e431ce590df3cef89688f854e51821d6a45
Andrew Buvria <buvrya228@gmail.com>
2023-03-28 21:03:25 +0200

    feat(m-user): create user interface which represent its dto and move it to shared

---
+++ libs/shared/src/interfaces/service-user-dto.interface.ts
@@ -0,0 +1,7 @@
+export interface ServiceUserDto {
+    readonly tgId: number;
+    readonly firstName: string;
+    readonly lastName: string;
+    readonly username: string;
+    readonly photoUrl: string;
+}
