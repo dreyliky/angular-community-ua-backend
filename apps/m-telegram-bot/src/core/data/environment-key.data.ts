f05b76cf4e4598f7536acbbf53c6a60db7e3f56e
dreyliky <dreyliky@gmail.com>
2023-05-20 22:00:35 +0300

    chore: m-telegram-bot env variable keys

--- apps/m-telegram-bot/src/core/data/environment-key.data.ts
+++ apps/m-telegram-bot/src/core/data/environment-key.data.ts
@@ -8,7 +8,6 @@ export const ENVIRONMENT_KEY = {
     Port: 'PORT',
     Production: 'PROD',
     BotToken: 'TELEGRAM_BOT_TOKEN',
-    AcuaWebAppUrl: 'ACUA_WEB_APP_URL',
     AcuaWebOverviewPageUrl: 'ACUA_WEB_OVERVIEW_PAGE_URL',
     AcuaGroupChatId: 'ACUA_GROUP_CHAT_ID',
     AcuaGroupCodeReviewThreadId: 'ACUA_GROUP_CODE_REVIEW_THREAD_ID'
